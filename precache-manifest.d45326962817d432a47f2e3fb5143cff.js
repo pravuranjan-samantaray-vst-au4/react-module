11d402c92e870d4b8e81971a8bfcb355f3cccfab
pravu_ps7@outlook.com <Pravu_ps7@outlook.com>
2020-07-03 18:08:00 +0530

    Updates

---
+++ precache-manifest.d45326962817d432a47f2e3fb5143cff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2129c28df3d4dcaf6a4c340217b357",
+    "url": "/react-module/index.html"
+  },
+  {
+    "revision": "b322718e949e7db2373a",
+    "url": "/react-module/static/css/main.d8cf7be6.chunk.css"
+  },
+  {
+    "revision": "72ef66e8fe35a11eb45a",
+    "url": "/react-module/static/js/2.ba6c7257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-module/static/js/2.ba6c7257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b322718e949e7db2373a",
+    "url": "/react-module/static/js/main.8992fd56.chunk.js"
+  },
+  {
+    "revision": "a2812ef4cd049957b09f",
+    "url": "/react-module/static/js/runtime-main.febf2006.js"
+  }
+]);
